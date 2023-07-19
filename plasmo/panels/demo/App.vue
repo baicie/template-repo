c8404fb1a239d939e1f93cd50d56a8621179c92c
刘志伟 <2055572879@qq.com>
2023-07-19 13:48:35 +0800

    feat: plasmo

---
+++ plasmo/panels/demo/App.vue
@@ -0,0 +1,16 @@
+<script lang='ts' setup>
+// # config import
+// # base state
+// # pinia
+// # computed
+// # watch
+// # function
+// # lifecycle
+// # style
+</script>
+
+<template>
+ <div>hello warld</div>
+</template>
+
+<style lang='scss' scoped></style>
---
+++ plasmo/popup.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { reactive } from 'vue'
+import fontPickerHTML from 'url:./panels/demo/index.html'
+
+// import fontPropertiesHTML from "url:./panels/font-properties/index.html"
+console.log('fontPickerHTML', fontPickerHTML)
+// chrome.devtools.panels.create(
+//   'Font Picker',
+//   null,
+//   // See: https://github.com/PlasmoHQ/plasmo/issues/106#issuecomment-1188539625
+//   fontPickerHTML.split('/').pop(),
+// )
+
+console.log('chrome', chrome)
+const state = reactive({ count: 0, action: null })
+
+function increment() {
+  state.count++
+  state.action = 'increment'
+}
+
+function decrement() {
+  state.count--
+  state.action = 'decrement'
+}
+</script>
+
+<template>
+  <div>
+    <h2 class="text-center">
+      Welcome to your
+      <a href="https://www.plasmo.com" target="_blank">Plasmo</a> Extension!
+    </h2>
+
+    <div class="container">
+      <button @click="decrement">
+        -
+      </button>
+      <p>
+        <b>{{ state.count }}</b>
+      </p>
+      <button @click="increment">
+        +
+      </button>
+    </div>
+  </div>
+  <p v-if="state.action" class="action text-center">
+    {{ state.action }}
+  </p>
+</template>
+
+<style>
+.container {
+  min-width: 470px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 47px;
+}
+.text-center {
+  text-align: center;
+}
+.action {
+  color: #470;
+  font-weight: bold;
+}
+</style>
